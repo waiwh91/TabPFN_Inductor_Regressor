2a05acfae7993e6d1e3071a63663dd4a24535aa3
waiwh91 <zcy12138@outlook.com>
2026-03-15 01:46:46 +0000

    TabPFN validation

--- cma_es_regressor.py
+++ cma_es_regressor.py
@@ -21,8 +21,8 @@ validation2 = pd.read_csv('csv/output.csv').to_numpy()
 
 
 
-R_r = TabPFNRegressor(device="cuda")
-L_r = TabPFNRegressor(device="cuda")
+R_r = TabPFNRegressor(device=torch.device('cuda'))
+L_r = TabPFNRegressor(device=torch.device('cuda'))
 
 
 
@@ -79,8 +79,8 @@ def forward(x: np.ndarray) -> np.ndarray:
 
     ######### TabPFN
 
-    ty1 = R_r.predict(x)
-    ty2 = L_r.predict(x)
+    # ty1 = R_r.predict(x)
+    # ty2 = L_r.predict(x)
 
     ######## PINN
     x = torch.from_numpy(np.log(x)).float()
@@ -88,16 +88,16 @@ def forward(x: np.ndarray) -> np.ndarray:
     y1, y2 = np.exp(model(x[:,:6],x[:,6]).T.detach().numpy())
 
 
-    return np.array([y1, y2, ty1, ty2], dtype=np.float64)
+    return np.array([y1, y2], dtype=np.float64)
 
 # --------- 逆向目标：给定 2 维目标指标 ---------
-y_target = np.array([300, 4,300,4], dtype=np.float64)
+y_target = np.array([300, 4], dtype=np.float64)
 
 
 
 # --------- 代价函数：让 forward(x) 接近 y_target ---------
 # 可加权、可加惩罚、可加正则（例如偏好更小的参数）
-w = np.array([1,1,0.6,0.6], dtype=np.float64)  # 两个指标的权重
+w = np.array([1,1], dtype=np.float64)  # 两个指标的权重
 
 def objective(x: np.ndarray) -> float:
 
@@ -120,8 +120,14 @@ def objective(x: np.ndarray) -> float:
     print(y)
 
     err = (y - y_target) * w
+
+    # diff = np.abs(y[0] -y[2]) * np.abs(y[0] -y[2])
+
+    # print("diff = ", diff)
+
     loss = float(np.dot(err, err))
     print("loss: ", loss)
+
     # 可选：让解更“温和”的正则（按需打开）
     # loss += 1e-3 * float(np.dot(x, x))
 
@@ -131,10 +137,10 @@ def objective(x: np.ndarray) -> float:
 opts = {
     "popsize": 24,                 # 种群大小（维度6通常 16~40 都行）
     "maxiter": 2000,                # 最大迭代
-    "tolfun": 4e-1,               # 收敛阈值
-    "tolx": 4e-1,
+    "tolfun": 4e-2,               # 收敛阈值
+    "tolx": 2e-2,
     "verb_disp": 20,               # 每隔多少代打印一次
-    "ftarget": 10,
+    "ftarget": 1,
     "bounds": [0, 1],
     "tolflatfitness": 50,
     "tolstagnation": 300,
@@ -144,48 +150,49 @@ opts = {
 
 flag = 0
 
+for i in range(20):
 
+    es = cma.CMAEvolutionStrategy(x0, sigma0, opts)
 
-es = cma.CMAEvolutionStrategy(x0, sigma0, opts)
-
-while not es.stop():
-    X = es.ask()                           # 采样一批候选 x
-    F = [objective(x) for x in X]          # 评估目标函数
-    es.tell(X, F)                          # 更新分布
-    es.disp()
-
-
-
-res = es.result
-x_best = np.array(res.xbest, dtype=np.float64)
+    while not es.stop():
+        X = es.ask()                           # 采样一批候选 x
+        F = [objective(x) for x in X]          # 评估目标函数
+        es.tell(X, F)                          # 更新分布
+        es.disp()
 
 
 
-y_best = forward(rounding(x_best))
-final_loss = objective(x_best)
-
-x_best = rounding(x_best)
-
-print("\n===== RESULT =====")
-print("y_target =", y_target)
-print("x_best   =", x_best)
-print("y_best   =", y_best)
-print("loss     =", final_loss)
-print("stop for", es.stop())
+    res = es.result
+    x_best = np.array(res.xbest, dtype=np.float64)
 
 
-# print(df.columns)
 
-print("=================")
-print("ANN value: ", forward(x_best))
+    y_best = forward(rounding(x_best))
+    final_loss = objective(x_best)
+
+    x_best = rounding(x_best)
+
+    print("\n===== RESULT =====")
+    print("y_target =", y_target)
+    print("x_best   =", x_best)
+    print("y_best   =", y_best)
+    print("loss     =", final_loss)
+    print("stop for", es.stop())
 
 
-df = pd.read_csv("csv/cma.csv")
+    # print(df.columns)
+
+    print("=================")
+    print("ANN value: ", forward(x_best))
 
 
-new_roll = {
-"tCu": x_best[0], "wCu": x_best[1], "tLam": x_best[2], "nLam": x_best[3],
-             "aln": x_best[4], "tsu": x_best[5], "freq": target_freq
-}
-df.loc[len(df)] = new_roll
-df.to_csv("csv/cma.csv", index=False)
+    df = pd.read_csv("csv/cma.csv")
+
+
+
+    new_roll = {
+    "tCu": x_best[0], "wCu": x_best[1], "tLam": x_best[2], "nLam": x_best[3],
+                 "aln": x_best[4], "tsu": x_best[5], "freq": target_freq
+    }
+    df.loc[len(df)] = new_roll
+    df.to_csv("csv/cma.csv", index=False)
--- test.py
+++ test.py
@@ -1,36 +1,6 @@
-import torch
-from models.PINN_inter_model import PINN
 import pandas as pd
-import numpy as np
 
-from tabpfn import TabPFNRegressor
-
-model = PINN()
-
-
-rr_reg = TabPFNRegressor(device=torch.device('cuda'))
-para_reg = TabPFNRegressor(device=torch.device('cuda'))
-para_l = TabPFNRegressor(device=torch.device('cuda'))
-
-
-
-validation = pd.read_csv('csv/pinn_data.csv').to_numpy()
-rr_reg.fit(validation[:,:7], validation[:,8])
-
-validation2 = pd.read_csv('csv/output.csv').to_numpy()
-para_reg.fit(validation2[:,:7], validation2[:,8])
-
-
-para_l.fit(validation2[:,:7], validation2[:,9])
-
-
-data = np.log(pd.read_csv('csv/cma.csv').to_numpy())
-data = np.exp(data)
-
-
-rr_p = rr_reg.predict(data[:,:7])
-para_p = para_reg.predict(data[:,:7])
-para_lp = para_l.predict(data[:,:7])
-
-
-print(para_p)
+df = pd.DataFrame(columns=[
+"tCu","wCu","tLam","nLam","aln","tsu","freq"
+])
+df.to_csv("csv/cma.csv", index=False)
